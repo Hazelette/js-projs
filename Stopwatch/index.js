a565f4a9535b1bc38ab3ac2a1f857dd7472110de
Hazelette <62589987+Hazelette@users.noreply.github.com>
2025-12-29 19:48:03 -0500

    Update Stopwatch function: get elapsed time using date

--- Stopwatch/index.js
+++ Stopwatch/index.js
@@ -5,7 +5,11 @@ let elapsedTime = 0;
 let isRunning = false;
 
 function start(){
-
+    if(!isRunning){
+        startTime = Date.now() - elapsedTime;
+        timer = setInterval(update, 10);
+        isRunning = true; 
+    }
 }
 
 function stop(){
@@ -17,5 +21,23 @@ function reset(){
 }
 
 function update(){
-    
+    const currentTime = Date.now();
+    elapsedTime = currentTime - startTime;
+
+    // convert to readable format
+    let hours = Math.floor(elapsedTime /(1000 * 60 * 60));
+    let minutes = Math.floor(elapsedTime / (1000 * 60) % 60); // reset when 60 
+    let seconds = Math.floor(elapsedTime / 1000 % 60);
+    let milliseconds = Math.floor(elapsedTime % 1000 / 10);
+
+    // Pad with 0 if only 1 digit
+    hours = String(hours).padStart(2, "0");
+    minutes = String(minutes).padStart(2, "0");
+    seconds = String(seconds).padStart(2, "0");
+    milliseconds = String(milliseconds).padStart(2, "0");
+
+    // change display
+    display.textContent = `${hours}:${minutes}:${seconds}:${milliseconds}`;
+
+
 }
