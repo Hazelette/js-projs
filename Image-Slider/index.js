eece74f6b936a6ecdc1265f357f0fc4ad168e4cd
Hazelette <62589987+Hazelette@users.noreply.github.com>
2026-01-06 16:42:06 +0800

    Slide next functionality and index reset added

--- Image-Slider/index.js
+++ Image-Slider/index.js
@@ -3,3 +3,39 @@
 // Nodelist of images
 const slides = document.querySelectorAll(".slides img");
 let slideIndex = 0;
+let intervalId = null;
+
+// initializeSlider();
+document.addEventListener("DOMContentLoaded", initializeSlider());
+
+function initializeSlider(){
+    if(slides.length > 0){
+        slides[slideIndex].classList.add("displaySlide");
+        intervalId = setInterval(nextSlide, 5000);
+    }
+}
+
+function showSlide(index){
+    if(index >= slides.length){
+        slideIndex = 0; // reset
+    }
+    else if(index < 0){
+        slideIndex = slides.length - 1;
+    }
+
+
+    // remove current slide
+    slides.forEach(slide => {
+        slide.classList.remove("displaySlide"); 
+    });
+    slides[slideIndex].classList.add("displaySlide");
+}
+
+function prevSlide(){
+
+}
+
+function nextSlide(){
+    slideIndex++;
+    showSlide(slideIndex);
+}
