a4eb6312d5a7d1f2f96ceff67969d01fa1d97878
Hazelette <62589987+Hazelette@users.noreply.github.com>
2026-01-06 15:33:13 +0800

    Wrote script for the RPS game

--- RPS/index.js
+++ RPS/index.js
@@ -7,6 +7,29 @@ const resultDisplay = document.getElementById("resultDisplay");
 
 function playGame(playerChoice){
     const computerChoice = choices[Math.floor(Math.random() * 3)];
+    let result = "";
 
-    console.log(computerChoice)
+    if (playerChoice === computerChoice){
+        result = "IT'S A TIE!";
+    }
+    else {
+        switch (playerChoice){
+            case "rock":
+                result = (computerChoice === "scissors") ? "YOU WIN!" : "YOU LOSE!";
+                break;
+            case "paper":
+                result = (computerChoice === "rock") ? "YOU WIN!" : "YOU LOSE!";
+                break;
+            case "scissors":
+                result = (computerChoice === "paper") ? "YOU WIN!" : "YOU LOSE!";
+                break;
+        }
+    }
+
+    playerDisplay.textContent = `PLAYER'S CHOICE: ${playerChoice}`;
+    computerDisplay.textContent = `COMPUTER'S CHOICE: ${computerChoice}`;
+    resultDisplay.textContent = result;
+
+
+    // console.log(computerChoice)
 }
