514e3ead50f41d0116d60ce7929487096f27d3c0
Hazelette <62589987+Hazelette@users.noreply.github.com>
2026-01-09 13:30:52 +0800

    Added function skeletons, deleted placeholders

---
+++ Weather-app/index.js
@@ -0,0 +1,25 @@
+// WEATHER APP
+const weatherForm = document.querySelector(".weatherForm");
+const cityInput = document.querySelector(".cityInput");
+const card = document.querySelector(".card");
+const apiKey = "4eac8f535ba51783ba2d6ac71ea7d66b";
+
+weatherForm.addEventListener("submit", event =>{
+    event.preventDefault();
+});
+
+async function  getWeather(city) {
+    
+}
+
+function displayWeatherInfo(data){
+
+}
+
+function getWeatherEmoji(weatherID){
+
+}
+
+function displayError(message){
+
+}
