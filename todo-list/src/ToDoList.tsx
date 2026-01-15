eea520236fea27d53647eee5b7f819201d476cc9
Hazelette <62589987+Hazelette@users.noreply.github.com>
2026-01-15 12:58:53 +0800

    fixed div issues and rearranged container

--- todo-list/src/ToDoList.tsx
+++ todo-list/src/ToDoList.tsx
@@ -23,39 +23,45 @@ function ToDoList() {
     <>
       <div className="todo-list">
         <h1>To-Do List</h1>
+
+        <div>
+          <input
+            type="text"
+            placeholder="Enter a task..."
+            value={newTask}
+            onChange={handleInputChange}
+          />
+
+          <button className="add-button" onClick={addTask}>
+            Add
+          </button>
+        </div>
+
+        <ol>
+          {tasks.map((tasks, index) => (
+            <li key={index}>
+              <span className="text">{tasks}</span>
+              <button
+                className="delete-button"
+                onClick={() => deleteTask(index)}
+              >
+                Delete
+              </button>
+
+              <button className="move-button" onClick={() => moveTaskUp(index)}>
+                ☝️
+              </button>
+
+              <button
+                className="move-button"
+                onClick={() => moveTaskDown(index)}
+              >
+                👇
+              </button>
+            </li>
+          ))}
+        </ol>
       </div>
-
-      <div>
-        <input
-          type="text"
-          placeholder="Enter a task..."
-          value={newTask}
-          onChange={handleInputChange}
-        />
-
-        <button className="add-button" onClick={addTask}>
-          Add
-        </button>
-      </div>
-
-      <ol>
-        {tasks.map((tasks, index) => (
-          <li key={index}>
-            <span className="text">{tasks}</span>
-            <button className="delete-button" onClick={() => deleteTask(index)}>
-              Delete
-            </button>
-
-            <button className="move-button" onClick={() => moveTaskUp(index)}>
-              ☝️
-            </button>
-
-            <button className="move-button" onClick={() => moveTaskDown(index)}>
-              ☝️
-            </button>
-          </li>
-        ))}
-      </ol>
     </>
   );
 }
