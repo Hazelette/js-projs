30af2d46e6f52f43236c2847a6091a7d0503a001
Hazelette <62589987+Hazelette@users.noreply.github.com>
2026-01-16 12:42:33 +0800

    Mount clock with usestate and useffect

--- clock-react/src/DigitalClock.tsx
+++ clock-react/src/DigitalClock.tsx
@@ -1,6 +1,19 @@
 import React, { useState, useEffect } from "react";
 
 function DigitalClock() {
+  const [time, setTime] = useState(new Date());
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setTime(new Date());
+    }, 1000); // callback, time
+
+    // when we unmount, clear interval to free resources
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []); // start timer only when we mount, not render so []
+
   return (
     <>
       <div className="clock-container">
